b7070000bd791cd74183d8a9833b93442f924e92
Victor Song <vsong1618@gmail.com>
2017-07-29 22:49:08 -0400

    Updated bash-snippets to v1.16.1

--- Formula/bash-snippets.rb
+++ Formula/bash-snippets.rb
@@ -1,7 +1,7 @@
 class BashSnippets < Formula
   desc "Collection of small bash scripts for heavy terminal users"
   homepage "https://github.com/alexanderepstein/Bash-Snippets"
-  url "https://github.com/alexanderepstein/Bash-Snippets/archive/v1.16.0.tar.gz"
+  url "https://github.com/alexanderepstein/Bash-Snippets/archive/v1.16.1.tar.gz"
   sha256 "19a029a204f16bd4db30cf7fa32aa6953dd10dcb996138b9bc65c01612afaac7"
 
   option "without-crypt", "Don't install crypt"
