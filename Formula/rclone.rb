1acdb7ec9c38903a39ba289e0632857116446a9c
Victor Song <vms2@rice.edu>
2020-11-17 20:56:49 -0600

    Update rclone.rb

--- Formula/rclone.rb
+++ Formula/rclone.rb
@@ -6,13 +6,6 @@ class Rclone < Formula
   license "MIT"
   head "https://github.com/rclone/rclone.git"
 
-  bottle do
-    cellar :any_skip_relocation
-    sha256 "3b50cc675b356f43b62f4f5a8acef082a56163ddf1dfdf03a4f360ee6969cdfe" => :catalina
-    sha256 "a59703acea34bb4bff3fcd878af83d425a993fc1012b35b7a4210066e6279eab" => :mojave
-    sha256 "cea1a5bf6e0346731ba8357e313ae6eb3633e14400c2e9e67bd5a3f8524721f6" => :high_sierra
-  end
-
   depends_on "go" => :build
 
   def install
